7cccf66500d402126b24aa0c76161f5b49485828
emklemkl <emil94_karlsson@outlook.com>
2024-10-01 13:49:53 +0200

    moved Document interface to interfaces/document

--- src/app/document.service.ts
+++ src/app/document.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-
+import { Document } from "@interfaces/document";
 // Define an interface for the document structure
-export interface Document {
-	name: string;
-}
 
 @Injectable({
 	providedIn: "root",
 })
 export class DocumentService {
-	private URL = " http://localhost:5000/document";
+	private URL =
+		"https://js-emlo-f6byg8hvbvhahgfp.northeurope-01.azurewebsites.net/document";
 	constructor(private http: HttpClient) {}
-	getAllDocuments(): Observable<any> {
-		return this.http.get(`${this.URL}/all`,)
+	getAllDocuments(): Observable<Document[]> {
+		return this.http.get<Document[]>(`${this.URL}/all`);
+	}
+	getDocument(id: number): Observable<Document[]> {
+		return this.http.get<Document[]>(`${this.URL}/${id}`);
 	}
 }
