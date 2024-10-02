1b42ee3758fb4a3081c86ec9a44b00ed4554975a
emklemkl <emil94_karlsson@outlook.com>
2024-10-02 18:55:37 +0200

    updated some visuals in the navbar, also add a create doc button/input

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
+import { Component } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DocumentCreateComponent } from "app/document-create/document-create.component";
 @Component({
-  selector: 'app-navigation',
-  standalone: true,
-  imports: [RouterModule],
-  // template: ``,
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+	selector: "app-navigation",
+	standalone: true,
+	imports: [RouterModule, DocumentCreateComponent],
+	// template: ``,
+	templateUrl: "./navigation.component.html",
+	styleUrl: "./navigation.component.scss",
 })
-export class NavigationComponent {
-
-}
+export class NavigationComponent {}
